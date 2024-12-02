6c003ef8ab10e9413627d915c1169821307e1c26
Aditya <adityakumar09@outlook.com>
2024-12-03 01:29:05 +0530

    sliding windown problem

---
+++ src/slidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,34 @@
+package slidingWindow;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        String str = "pwwkew";
+        int ans = lengthOfLongestSubstring(str);
+        System.out.println(ans);
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+
+        while(right < s.length()) {
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c,0) + 1);
+
+            while(map.get(c) > 1) {
+                char cc = s.charAt(left);
+                map.put(cc, map.get(cc) - 1);
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
+    }
+}
